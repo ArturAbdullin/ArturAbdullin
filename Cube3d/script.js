029fd29f5b6038ac0ebef24ab6b360681fffecc8
Artur Abdullin <artur.abdullin@gmail.com>
2022-01-03 17:39:22 +0300

    last working version

--- Cube3d/script.js
+++ Cube3d/script.js
@@ -3,6 +3,78 @@ let cube = document.querySelector(".cube");
 let pi = Math.PI;
 let origin = [1, 0, 0, 0, 1, 0, 0, 0, 1];
 cube.style.transform = array2DOMtransform(origin);
+class cube {
+  constructor() {
+    this.origin = [1, 0, 0, 0, 1, 0, 0, 0, 1];
+  }
+
+  getOrigin() {
+    return this.origin;
+  }
+
+  rotate(vector, angle) {
+    const x = vector[0];
+    const y = vector[1];
+    const z = vector[2];
+    const [xx, yy, zz, xy, xz, yz] = [x * x, y * y, z * z, x * y, x * z, y * z];
+
+    const ca = Math.cos(angle);
+    const ca1 = 1 - ca;
+    const sa = Math.sin(angle);
+    const sa1 = 1 - sa;
+
+    const rotMatrix = [
+      xx * ca1 + ca,
+      xy * ca1 - z * sa,
+      xz * ca1 + y * sa,
+      xy * ca1 + z * sa,
+      yy * ca1 + ca,
+      yz * ca1 - x * sa,
+      xz * ca1 - y * sa,
+      yz * ca1 + x * sa,
+      zz * ca1 + ca,
+    ];
+
+    const m11 =
+      rotMatrix[0] * this.origin[0] +
+      rotMatrix[1] * this.origin[3] +
+      rotMatrix[2] * this.origin[6];
+    const m12 =
+      rotMatrix[0] * this.origin[1] +
+      rotMatrix[1] * this.origin[4] +
+      rotMatrix[2] * this.origin[7];
+    const m13 =
+      rotMatrix[0] * this.origin[2] +
+      rotMatrix[1] * this.origin[5] +
+      rotMatrix[2] * this.origin[8];
+    const m21 =
+      rotMatrix[3] * this.origin[0] +
+      rotMatrix[4] * this.origin[3] +
+      rotMatrix[5] * this.origin[6];
+    const m22 =
+      rotMatrix[3] * this.origin[1] +
+      rotMatrix[4] * this.origin[4] +
+      rotMatrix[5] * this.origin[7];
+    const m23 =
+      rotMatrix[3] * this.origin[2] +
+      rotMatrix[4] * this.origin[5] +
+      rotMatrix[5] * this.origin[8];
+    const m31 =
+      rotMatrix[6] * this.origin[0] +
+      rotMatrix[7] * this.origin[3] +
+      rotMatrix[8] * this.origin[6];
+    const m32 =
+      rotMatrix[6] * this.origin[1] +
+      rotMatrix[7] * this.origin[4] +
+      rotMatrix[8] * this.origin[7];
+    const m33 =
+      rotMatrix[6] * this.origin[2] +
+      rotMatrix[7] * this.origin[5] +
+      rotMatrix[8] * this.origin[8];
+
+    this.origin = [m11, m12, m13, m21, m22, m23, m31, m32, m33];
+  }
+}
 
 function rotate(origin, vector, angle) {
   let x = vector[0];
@@ -27,15 +99,42 @@ function rotate(origin, vector, angle) {
     zz * ca1 + ca,
   ];
 
-  let m11 = rotMatrix[0] * origin[0] + rotMatrix[1] * origin[3] + rotMatrix[2] * origin[6];
-  let m12 = rotMatrix[0] * origin[1] + rotMatrix[1] * origin[4] + rotMatrix[2] * origin[7];
-  let m13 = rotMatrix[0] * origin[2] + rotMatrix[1] * origin[5] + rotMatrix[2] * origin[8];
-  let m21 = rotMatrix[3] * origin[0] + rotMatrix[4] * origin[3] + rotMatrix[5] * origin[6];
-  let m22 = rotMatrix[3] * origin[1] + rotMatrix[4] * origin[4] + rotMatrix[5] * origin[7];
-  let m23 = rotMatrix[3] * origin[2] + rotMatrix[4] * origin[5] + rotMatrix[5] * origin[8];
-  let m31 = rotMatrix[6] * origin[0] + rotMatrix[7] * origin[3] + rotMatrix[8] * origin[6];
-  let m32 = rotMatrix[6] * origin[1] + rotMatrix[7] * origin[4] + rotMatrix[8] * origin[7];
-  let m33 = rotMatrix[6] * origin[2] + rotMatrix[7] * origin[5] + rotMatrix[8] * origin[8];
+  let m11 =
+    rotMatrix[0] * origin[0] +
+    rotMatrix[1] * origin[3] +
+    rotMatrix[2] * origin[6];
+  let m12 =
+    rotMatrix[0] * origin[1] +
+    rotMatrix[1] * origin[4] +
+    rotMatrix[2] * origin[7];
+  let m13 =
+    rotMatrix[0] * origin[2] +
+    rotMatrix[1] * origin[5] +
+    rotMatrix[2] * origin[8];
+  let m21 =
+    rotMatrix[3] * origin[0] +
+    rotMatrix[4] * origin[3] +
+    rotMatrix[5] * origin[6];
+  let m22 =
+    rotMatrix[3] * origin[1] +
+    rotMatrix[4] * origin[4] +
+    rotMatrix[5] * origin[7];
+  let m23 =
+    rotMatrix[3] * origin[2] +
+    rotMatrix[4] * origin[5] +
+    rotMatrix[5] * origin[8];
+  let m31 =
+    rotMatrix[6] * origin[0] +
+    rotMatrix[7] * origin[3] +
+    rotMatrix[8] * origin[6];
+  let m32 =
+    rotMatrix[6] * origin[1] +
+    rotMatrix[7] * origin[4] +
+    rotMatrix[8] * origin[7];
+  let m33 =
+    rotMatrix[6] * origin[2] +
+    rotMatrix[7] * origin[5] +
+    rotMatrix[8] * origin[8];
 
   return [m11, m12, m13, m21, m22, m23, m31, m32, m33];
 }
@@ -46,32 +145,32 @@ function array2DOMtransform(array) {
 
 function turn(origin, direction) {
   switch (direction) {
-    case 'up':
-      return rotate(origin, [1, 0, 0], pi/2);
-    case 'down':
-      return rotate(origin, [1, 0, 0], -pi/2);
-    case 'left':
-      return rotate(origin, [0, 1, 0], -pi/2);
-    case 'right':
-      return rotate(origin, [0, 1, 0], pi/2);
+    case "up":
+      return rotate(origin, [1, 0, 0], pi / 2);
+    case "down":
+      return rotate(origin, [1, 0, 0], -pi / 2);
+    case "left":
+      return rotate(origin, [0, 1, 0], -pi / 2);
+    case "right":
+      return rotate(origin, [0, 1, 0], pi / 2);
     default:
       return origin;
   }
 }
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener("keydown", function (e) {
   switch (e.key) {
-    case 'ArrowUp':
-      origin = turn(origin, 'up');
+    case "ArrowUp":
+      origin = turn(origin, "up");
       break;
-    case 'ArrowDown':
-      origin = turn(origin, 'down');
+    case "ArrowDown":
+      origin = turn(origin, "down");
       break;
-    case 'ArrowLeft':
-      origin = turn(origin, 'left');
-    break;
-    case 'ArrowRight':
-      origin = turn(origin, 'right');
+    case "ArrowLeft":
+      origin = turn(origin, "left");
+      break;
+    case "ArrowRight":
+      origin = turn(origin, "right");
       break;
     default:
       break;
@@ -79,19 +178,37 @@ document.addEventListener('keydown', function(e) {
   cube.style.transform = array2DOMtransform(origin);
 });
 
+class cubeInterface {
+  constructor(cube) {
+    this.cube = cube;
+    this.DomElement;
+  }
+
+  assignDomElement(element) {
+    this.DomElement = element;
+  }
+
+  updateTransform() {
+    this.DomElement.style.transform = array2DOMtransform();
+  }
+
+  array2DOMtransform() {
+    return `matrix3d(${this.origin[0]}, ${this.origin[3]}, ${this.origin[6]}, 0, ${this.origin[1]}, ${this.origin[4]}, ${this.origin[7]}, 0, ${this.origin[2]}, ${this.origin[5]}, ${this.origin[8]}, 0, 0, 0, 0, 1)`;
+  }
+}
 
 let turned = false;
-document.addEventListener('mousedown', handleMouseDown);
-document.addEventListener('mouseup', handleMouseUp);
-document.addEventListener('touchstart', handleTouchStart);
-document.addEventListener('touchend', handleTouchEnd);
+document.addEventListener("mousedown", handleMouseDown);
+document.addEventListener("mouseup", handleMouseUp);
+document.addEventListener("touchstart", handleTouchStart);
+document.addEventListener("touchend", handleTouchEnd);
 
 function handleMouseDown(e) {
-  document.addEventListener('mousemove', handleMouseMove);
+  document.addEventListener("mousemove", handleMouseMove);
 }
 
 function handleMouseUp(e) {
-  document.removeEventListener('mousemove', handleMouseMove);
+  document.removeEventListener("mousemove", handleMouseMove);
   turned = false;
 }
 
@@ -113,27 +230,27 @@ function checkDirection(x, y, sensitivity) {
   let yabs = Math.abs(y);
   if (xabs >= yabs) {
     if (xabs >= sensitivity) {
-      return x > 0 ? 'right' : 'left';
+      return x > 0 ? "right" : "left";
     }
   } else if (yabs >= sensitivity) {
-    return y > 0 ? 'down' : 'up';
+    return y > 0 ? "down" : "up";
   }
   return null;
 }
 
 let touch = {
   x: 0,
-  y: 0
-}
+  y: 0,
+};
 
 function handleTouchStart(e) {
   touch.x = e.touches[0].clientX;
   touch.y = e.touches[0].clientY;
-  document.addEventListener('touchmove', handleTouchMove);
+  document.addEventListener("touchmove", handleTouchMove);
 }
 
 function handleTouchEnd(e) {
-  document.removeEventListener('touchmove', handleTouchMove);
+  document.removeEventListener("touchmove", handleTouchMove);
   turned = false;
   [touch.x, touch.y] = [0, 0];
 }
